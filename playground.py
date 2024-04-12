dd45540acd93ee181d041d1948ebf5c695474718
Jean Charle Yaacoub <jyaacoub21@gmail.com>
2024-04-12 09:45:58 -0400

    feat: FSDP implementation
    FSDP is good for low mem training to distribute params across devices, but not really the best for inference.

--- playground.py
+++ playground.py
@@ -1,3 +1,4 @@
+#%%
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_csv', type=str, default='splits/transporters_only.csv')
@@ -25,14 +26,26 @@ parser.add_argument('--low_mem', action='store_true', default=False)
 parser.add_argument('--rank', type=int, default=0)
 parser.add_argument('--world_size', type=int, default=1)
 
-args = parser.parse_args()
+io_dir="/home/jyaacoub/projects/def-sushant/jyaacoub/data/pdbbind/alphaflow_io/"
+
+args = parser.parse_args(args=f"--mode alphafold \
+                  --input_csv {io_dir}/input_19.csv \
+                  --msa_dir {io_dir}/msa_dir \
+                  --weights weights/alphaflow_md_distilled_202402.pt \
+                  --samples 50 \
+                  --outpdb {io_dir}/out_pdb_MD-distilled \
+                  --no_overwrite \
+                  --low_mem \
+                  \
+                  --rank 0 \
+                  --world_size 2".split())
 
 import functools
 import torch, tqdm, os, json, time
 import numpy as np
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp import CPUOffload
-from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy, wrap, always_wrap_policy
+from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy, wrap, always_wrap_policy, transformer_auto_wrap_policy
 
 from collections import defaultdict
 from alphaflow.data.data_modules import collate_fn
@@ -45,7 +58,7 @@ from alphaflow.config import model_config
 
 from alphaflow.utils.logging import get_logger
 logger = get_logger(__name__)
-torch.set_float32_matmul_precision("medium") # for low mem
+torch.set_float32_matmul_precision("medium")
 
 config = model_config(
     'initial_training',
@@ -62,7 +75,7 @@ data_cfg.common.max_recycling_iters = 0
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'  # Use a free port
+    os.environ['MASTER_PORT'] = '12356'  # Use a free port
 
     # Initializes the default distributed process group, and this will also initialize the distributed package
     torch.distributed.init_process_group("nccl", rank=rank, world_size=world_size)
@@ -83,25 +96,27 @@ def custom_autowrap_policy(module, recurse, unwrapped_params):
     # return isinstance(module, torch.nn.Module) and ('ExtraMSAStack' in module.__class__.__name__ 
     #                                                    or 'EvoformerStack' in module.__class__.__name__
     #                                                    or 'InputPairStack' in module.__class__.__name__)
-@torch.no_grad()
-def main():
-    if args.rank != 0:
-        time.sleep(1)
-    print(args.rank, args.world_size)
-    setup(args.rank, args.world_size)
-    
-    ################## GET DATASET ##################
-    valset = {
-        'alphafold': AlphaFoldCSVDataset,
-        'esmfold': CSVDataset,
-    }[args.mode](
-        data_cfg,
-        args.input_csv,
-        msa_dir=args.msa_dir,
-        data_dir=args.templates_dir,
-        templates=args.templates,
-    )
-    
+
+# %%
+if args.rank != 0:
+    time.sleep(1)
+print(args.rank, args.world_size)
+setup(args.rank, args.world_size)
+
+################## GET DATASET ##################
+valset = {
+    'alphafold': AlphaFoldCSVDataset,
+    'esmfold': CSVDataset,
+}[args.mode](
+    data_cfg,
+    args.input_csv,
+    msa_dir=args.msa_dir,
+    data_dir=args.templates_dir,
+    templates=args.templates,
+)
+
+#%%
+with torch.no_grad():
     torch.cuda.set_device(args.rank)
     ################## LOAD MODEL ##################
     logger.info("Loading the model")
@@ -118,8 +133,7 @@ def main():
     # wrap model with FSDP
     #%%
     model = FSDP(model, 
-                    device_id=args.rank, # mutually exclusive with cpu_offload
-                    # cpu_offload=CPUOffload(offload_params=True),
+                    cpu_offload=CPUOffload(offload_params=True),
                     # limit_all_gathers=True, # only in v2.0+
                     auto_wrap_policy=custom_autowrap_policy,
                     # auto_wrap_policy=always_wrap_policy,
@@ -127,50 +141,24 @@ def main():
                     #        size_based_auto_wrap_policy,
                     #        min_num_params=int(1e8))
                 )
-    
+
     model.eval()
     # barrier to ensure all processes have loaded the model
     torch.distributed.barrier()
-    
+    # %%
+
     logger.info("Model has been loaded")
     ################## INFERENCE ##################
     if args.rank == 0: os.makedirs(args.outpdb, exist_ok=True)
-    runtime = defaultdict(list)
-    for i, item in enumerate(valset):
-        if (args.pdb_id and item['name'] not in args.pdb_id):
-            continue
-        
-        out_fp = f'{args.outpdb}/{item["name"]}.pdb'
-        if os.path.exists(out_fp) and args.no_overwrite:
-            if args.rank == 0: print(f"{i}:{item['name']} already exists, skipping...")
-            continue
-        result = []
-        for _ in tqdm.trange(args.samples, disable=args.rank != 0):
-            batch = collate_fn([item])
-            batch = tensor_tree_map(lambda x: x.cuda(), batch)  
-            start = time.time()
-            torch.distributed.barrier()
-            prots = model.inference(batch, as_protein=True, noisy_first=args.noisy_first,
-                        no_diffusion=args.no_diffusion, schedule=schedule, self_cond=args.self_cond)
-            runtime[item['name']].append(time.time() - start)
-            result.append(prots[-1])
-        
-        # Save pdb
-        if args.rank == 0:
-            with open(out_fp, 'w') as f:
-                f.write(protein.prots_to_pdb(result))
 
-    if args.runtime_json and args.rank == 0:
-        with open(args.runtime_json, 'w') as f:
-            f.write(json.dumps(dict(runtime)))
-    
-    cleanup()
-
-try:
-    main()
-except Exception as e:
-    if args.rank == 0:
-        raise e
-    time.sleep(1)
-    print(f"rank {args.rank} also ran into exception '{e}'")
-
+    item = valset[92] #valset[0]
+    result = []
+    for j in tqdm.trange(args.samples, disable=args.rank != 0):
+        batch = collate_fn([item])
+        batch = tensor_tree_map(lambda x: x.cuda(), batch)  
+        start = time.time()
+        torch.distributed.barrier()
+        prots = model.inference(batch, as_protein=True, noisy_first=args.noisy_first,
+                    no_diffusion=args.no_diffusion, schedule=schedule, self_cond=args.self_cond)
+        result.append(prots[-1])
+    # %%
